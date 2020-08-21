5cc7c33a7249e0eb14149b99b180c41e46005792
Janet Karpenske <janetkarpenske@gmail.com>
2020-08-21 09:29:09 -0700

    Pass 5th test to respect hierarchy in which 3 is most important, then 2, then 1, etc

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,16 @@ function roboger(num) {
   }
   let j = 0;
   numArray.forEach(function(number) {
-    if (number.indexOf('1') > -1) {
-      numArray[j] = "Beep!";
+    if (number.indexOf('3') > -1) {
+      numArray[j] = "Won't you be my neighbor?";
     }
     else if (number.indexOf('2') > -1) {
       numArray[j] = "Boop!";
     }
-    else if (number.indexOf('3') > -1) {
-      numArray[j] = "Won't you be my neighbor?";
+    else if (number.indexOf('1') > -1) {
+      numArray[j] = "Beep!";
     }
+    else {}
     j++
   })
   return numArray.join(", ")
