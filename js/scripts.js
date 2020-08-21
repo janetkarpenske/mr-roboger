a09b025628241b5b1c392dd4e78d09c0331989aa
Janet Karpenske <janetkarpenske@gmail.com>
2020-08-21 09:20:12 -0700

    pass second test to have any number containing a 1 return beep instead

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
-
-
-
 function roboger(num) {
   let numArray = []
   for (let i = 0; i <= num; i++) {
-    numArray.push(i);
+    numArray.push(i.toString());
   }
-
+  let j = 0;
+  numArray.forEach(function(number) {
+    if (number.indexOf('1') > -1) {
+      numArray[j] = "Beep!";
+      console.log("There's a 1 here");
+    }
+    j++
+  })
   return numArray.join(", ")
 }
 
-console.log(roboger(10));
+console.log(roboger(30));
